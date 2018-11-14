5dddffa62a3f26c536a6b76a06686f996c9c5820
3A532067 <s3a532067@student.ncut.edu.tw>
2018-11-14 11:33:55 +0800

    練習二:改用 new 的方式新增資料

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,18 @@
 
 Route::get('/', function () {
 //    return view('welcome');
-    \App\Post::create([
-        'title'=>'test title',
-        'content'=>'test content',
-    ]);
+
+    
+//    \App\Post::create([
+//        'title'=>'test title',
+//        'content'=>'test content',
+//    ]);
+
+
+    $post =new\App\Post();
+    $post ->title = 'test title';
+    $post ->content = 'test content';
+    $post ->save();
+
 
 });
