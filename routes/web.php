1b6b1949bf35b35e494c40610084e5392bf0d280
3A532067 <s3a532067@student.ncut.edu.tw>
2018-11-14 13:31:54 +0800

    練習六:取得 Model

--- routes/web.php
+++ routes/web.php
@@ -59,8 +59,14 @@ Route::get('/', function () {
 
 //    \App\Post::destroy(3,5,7);
 
-    $allPosts=\App\Post::all();
-    dd($allPosts);
-    $featuredPosts=\App\Post::where('is_feature',1)->get();
-    dd($featuredPosts);
+//    $allPosts=\App\Post::all();
+//    dd($allPosts);
+//    $featuredPosts=\App\Post::where('is_feature',1)->get();
+//    dd($featuredPosts);
+
+    $fourthPost=\App\Post::find(4);
+    dd($fourthPost);
+    $lastPost=\App\Post::orderBy('id','DESC')->first();
+    dd($lastPost);
+
 });
