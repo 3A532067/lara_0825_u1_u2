d638311bbd059711404a05b10c5c47849effa8fb
3A532067 <s3a532067@student.ncut.edu.tw>
2018-11-21 01:19:15 +0800

    練習七:3開啟 Comment.php，增加關聯

--- app/Comment.php
+++ app/Comment.php
@@ -4,7 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Post;
+
 class Comment extends Model
 {
     protected $table= 'comments';
+
+public function post()
+{
+return $this->belongsTo(Post::class);
+}
+
 }
