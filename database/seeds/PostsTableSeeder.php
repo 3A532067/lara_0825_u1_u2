d55e116665ffcf5190f75ace1203464cc08c5eb1
3A532067 <s3a532067@student.ncut.edu.tw>
2018-11-28 18:30:29 +0800

    練習 4：調整 seeder 產生的資料(每次 seeding 前先清空資料表 & 讓資料有時間序列性、隨機感 & 使用 Faker 讓資料更擬真)

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use App\Post;
+use Faker\Factory as Faker;
+use Carbon\Carbon;
 
 class PostsTableSeeder extends Seeder
 {
@@ -13,12 +15,27 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         //
+
+        Post::truncate();
+
         foreach(range(1,20)as$number){
+
+            $total=20;
+            $faker=Faker::create('zh_TW');
             Post::create([
-                'title'=>'title'.$number,
-                'content'=>'content'.$number,
+//                'title'=>'title'.$number,
+//                'content'=>'content'.$number,
+
+                  'title'=>$faker->sentence,
+                  'content'=>$faker->paragraph,
+                  'is_feature'=>rand(0,1),
+                  'created_at'=>Carbon::now()->subDays($total-$number),
+                  'updated_at'=>Carbon::now()->subDays($total-$number),
+
             ]);
+
         }
+
 
     }
 }
